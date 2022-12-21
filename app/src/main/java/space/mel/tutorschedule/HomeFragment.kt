d417564461a71c1f1d8812c6f8d3c396ad475659
Slayer-mel <valera83mel@gmail.com>
2022-12-21 12:25:57 +0200

    Add Room

--- app/src/main/java/space/mel/tutorschedule/HomeFragment.kt
+++ app/src/main/java/space/mel/tutorschedule/HomeFragment.kt
@@ -1,20 +1,20 @@
 package space.mel.tutorschedule
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import space.mel.tutorschedule.databinding.FragmentHomeBinding
+import space.mel.tutorschedule.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
-    lateinit var homeBinding: FragmentHomeBinding
+    lateinit var homeBinding: HomeFragmentBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        homeBinding = FragmentHomeBinding.inflate(layoutInflater)
+        homeBinding = HomeFragmentBinding.inflate(layoutInflater)
         return homeBinding.root
     }
 
--- app/src/main/java/space/mel/tutorschedule/MainActivity.kt
+++ app/src/main/java/space/mel/tutorschedule/MainActivity.kt
@@ -1,10 +1,9 @@
 package space.mel.tutorschedule
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
 import space.mel.tutorschedule.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,13 +15,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
-        val navHostFragment =
+        //setupActionBarWithNavController(findNavController(R.id.listFragment))
+        /*val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
         navController = navHostFragment.navController
 
         if (savedInstanceState == null) {
             navigateToFragment(HomeFragment())
-        }
+        }*/
     }
     private fun navigateToFragment(fragment: Fragment) {
         //navController.navigate(R.id.action_homeFragment_to_findInfoFragment2)
